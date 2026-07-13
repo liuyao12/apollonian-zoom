73cfa20d76458aeed7a6460509f7f75475056820
Yao Liu <liuyao@gmail.com>
2026-07-12 23:16:09 -0700

    Add integral Apollonian packing presets

---
+++ src/presets.js
@@ -0,0 +1,14 @@
+export const presets = {
+  "Classic": [
+    {b:-1n,bx:0n,by:0n},
+    {b:2n,bx:-1n,by:0n},
+    {b:2n,bx:1n,by:0n},
+    {b:2n,bx:0n,by:2n}
+  ],
+  "Small outer circle": [
+    {b:-3n,bx:0n,by:0n},
+    {b:4n,bx:-2n,by:0n},
+    {b:4n,bx:2n,by:0n},
+    {b:12n,bx:0n,by:6n}
+  ]
+};
