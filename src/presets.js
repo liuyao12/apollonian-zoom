6e57ccf19c4429c81cf8cb22f455eb7ac90a0fa2
Yao Liu <liuyao@gmail.com>
2026-07-12 23:22:15 -0700

    Fix Apollonian presets with valid Descartes configurations

--- src/presets.js
+++ src/presets.js
@@ -1,14 +1,14 @@
 export const presets = {
-  "Classic": [
+  "Classic (-1,2,2,3)": [
     {b:-1n,bx:0n,by:0n},
     {b:2n,bx:-1n,by:0n},
     {b:2n,bx:1n,by:0n},
-    {b:2n,bx:0n,by:2n}
+    {b:3n,bx:0n,by:2n}
   ],
-  "Small outer circle": [
-    {b:-3n,bx:0n,by:0n},
-    {b:4n,bx:-2n,by:0n},
-    {b:4n,bx:2n,by:0n},
-    {b:12n,bx:0n,by:6n}
+  "Rotated (-1,2,2,3)": [
+    {b:-1n,bx:0n,by:0n},
+    {b:2n,bx:0n,by:-1n},
+    {b:2n,bx:0n,by:1n},
+    {b:3n,bx:2n,by:0n}
   ]
 };
